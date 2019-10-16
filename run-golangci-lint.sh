6ebe84bb004ff98edfd5575dd6e624d6e064d70d
FelixSeptem <whenhow94@gmail.com>
2019-10-16 15:52:27 +0800

    update cmd

--- run-golangci-lint.sh
+++ run-golangci-lint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec golangci-lint run --enable goimports --enable gocritic --skip-dirs ./vendor
+exec golangci-lint run --deadline=10m --enable goimports --enable misspell --enable gocritic --skip-dirs ./vendor --out-format=line-number
