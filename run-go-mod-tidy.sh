12f5442f51377b10b26651ad745206bbe1500ad6
FelixSeptem <whenhow94@gmail.com>
2020-07-17 16:23:13 +0800

    add sheban

--- run-go-mod-tidy.sh
+++ run-go-mod-tidy.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
 go mod tidy
+git diff --quiet go.mod
+is_go_mod_changed=$?
+git diff --quiet go.sum
+is_go_sum_changed=$?
+
+if (( $is_go_mod_changed || $is_go_sum_changed )); then
+  exit 1
+fi
