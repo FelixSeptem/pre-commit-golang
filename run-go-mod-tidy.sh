48688d3ca85c3c2f6858e2c9ff5a7fdf54117aea
FelixSeptem <whenhow94@gmail.com>
2020-07-17 11:43:03 +0800

    add shell file

---
+++ run-go-mod-tidy.sh
@@ -0,0 +1,10 @@
+go mod tidy
+git diff --quiet go.mod
+is_go_mod_changed=$?
+git diff --quiet go.sum
+is_go_sum_changed=$?
+
+if (( $is_go_mod_changed || $is_go_sum_changed )); then
+  git status
+  exit 1
+fi
